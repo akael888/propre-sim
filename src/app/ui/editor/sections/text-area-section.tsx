72d59c4b7453a6ede09267443b472c18f9233d00
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-19 00:53:52 +0900

    feat : Added Ordering When in Small Screen

--- src/app/ui/editor/sections/text-area-section.tsx
+++ src/app/ui/editor/sections/text-area-section.tsx
@@ -11,7 +11,7 @@ const TextAreaSection: React.FC<TextAreaSectionProps> = ({
 }) => {
   return (
     <>
-      <div className="w-full h-full flex justify-center items-center border-1 p-1 overflow-y-auto">
+      <div className="w-full h-full flex justify-center items-center border-1 p-1 overflow-y-auto md:order-none order-2">
         <TextArea
           textData={textData}
           handleTextDataChanges={handleTextDataChanges}
