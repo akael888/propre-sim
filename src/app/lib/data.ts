6a3c44fdbcd786a55a03fae7ad9a1573eaa32135
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-19 12:41:44 +0900

    feat : Added Text Align Type Enum and Text Attribute Object

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,16 +1,1 @@
-export function parseTextDataToObjects(textData: string) {
-  console.log(textData);
-
-  const splittedData = textData.split("\n\n");
-
-  console.log(splittedData);
-
-  const splittedObject = splittedData.map((data, index) => {
-    return { id: index, content: data };
-  });
-
-  console.log(splittedObject);
-  console.log("test")
-
-  return splittedObject;
-}
+export type textAlignTypes = "left" | "center" | "right" | "justify";
---
+++ src/app/lib/utils.ts
@@ -0,0 +1,17 @@
+import { textAlignTypes } from "./data";
+export function parseTextDataToObjects(textData: string) {
+  console.log(textData);
+
+  const splittedData = textData.split("\n\n");
+
+  console.log(splittedData);
+
+  const splittedObject = splittedData.map((data, index) => {
+    return { id: index, content: data };
+  });
+
+  console.log(splittedObject);
+  console.log("test");
+
+  return splittedObject;
+}
