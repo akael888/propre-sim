2a7082b4bc5f5dc69494f4d0098476942873290b
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-20 15:09:56 +0900

    chore : Moved All Interface into Type.ts

--- src/app/lib/type.ts
+++ src/app/lib/type.ts
@@ -1,3 +1,4 @@
+import { RefObject } from "react";
 import { textAlignTypes } from "./data";
 
 export interface TextAttribute {
@@ -9,5 +10,47 @@ export interface TextAttribute {
 export interface TextObject {
   id: number;
   content: string;
-  clicked: boolean;
+  charIndex: number;
+}
+
+// Text Area -----------------------------
+export interface TextAreaSectionProps {
+  textData: string;
+  handleTextDataChanges: (textData: string) => void;
+  textAreaRef: RefObject<HTMLTextAreaElement | null>;
+}
+
+export interface TextAreaProps {
+  textData: string;
+  handleTextDataChanges: (textData: string) => void;
+  textAreaRef: RefObject<HTMLTextAreaElement | null>;
+}
+
+// Display Area----------------
+export interface DisplayAreaSectionProps {
+  slideObject: TextObject[];
+  textAreaRef: RefObject<HTMLTextAreaElement | null>;
+}
+
+export interface DisplayPanelProps {
+  slideObject: TextObject[];
+  textAttribute: TextAttribute;
+  textAreaRef: RefObject<HTMLTextAreaElement | null>;
+}
+
+export interface SlideProps {
+  slideNum: number;
+  slideMaxNum: number;
+  slideContent: string;
+  slideTextCharIndex: number;
+  textAttribute: TextAttribute;
+  textAreaRef: RefObject<HTMLTextAreaElement | null>;
+}
+
+export interface DisplayOptionsProp {
+  textAttribute: TextAttribute;
+  handleTextAttributeChanges: <K extends keyof TextAttribute>(
+    attribute: K,
+    attributeValue: TextAttribute[K],
+  ) => void;
 }
