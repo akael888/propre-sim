738e318b0e7ece21b4bd42540727d72c9a696a0f
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-20 15:13:10 +0900

    chore : Clean Up Interface and Added Click Text View Function

--- src/app/ui/editor/display/slide.tsx
+++ src/app/ui/editor/display/slide.tsx
@@ -1,31 +1,52 @@
-import { TextAttribute, TextObject } from "@/app/lib/type";
-interface SlideProps {
-  slideNum: number;
-  slideMaxNum: number;
-  slideContent: string;
-  textAttribute: TextAttribute;
-  handleSlideObjectChanges: <K extends keyof TextObject>(
-    index: number,
-    objectKey: K,
-    objectValue: TextObject[K],
-  ) => void;
-}
+import { SlideProps, TextAttribute, TextObject } from "@/app/lib/type";
 
 const Slide: React.FC<SlideProps> = ({
   slideNum,
   slideMaxNum,
   slideContent,
+  slideTextCharIndex,
   textAttribute,
-  handleSlideObjectChanges,
+  textAreaRef,
 }) => {
   // convertTextAlignEnumToCSS(textAttribute.textAlign);
+
+  const FocusOnTextArea = (textSearch: string) => {
+    const textarea = textAreaRef.current;
+    if (!textarea) {
+      return;
+    }
+
+    const text = textarea.value; //All Text Area Values
+
+    // Spliced Text from the Latest Char Index, to Make sure it wont highlight duplicated ones
+    const splicedTextFromCurrentCharIndex = text.substring(
+      slideTextCharIndex,
+      slideTextCharIndex + textSearch.length,
+    );
+
+    // Set the target index
+    const targetIndex =
+      splicedTextFromCurrentCharIndex.indexOf(textSearch) + slideTextCharIndex;
+
+    if (targetIndex !== 1) {
+      textarea.focus();
+      textarea.setSelectionRange(targetIndex, targetIndex + textSearch.length);
+
+      const lines = text.substring(0, targetIndex).split("\n").length; //Calculcate how many enter/lines in the text
+      const lineHeight = 24; //approx each line height
+      const scrollPosition = (lines - 10) * lineHeight; //-3 to give more context above
+
+      textarea.scrollTop = Math.max(0, scrollPosition);
+    }
+  };
+
   return (
     <>
       <div
+        id={slideContent}
         className="aspect-video border-1 bg-pink-200 flex justify-center items-center relative overflow-hidden hover:bg-pink-100"
         style={{ containerType: "inline-size" }}
-        onClick={() => handleSlideObjectChanges(slideNum, "clicked", true)}
-        onKeyUp={() => handleSlideObjectChanges(slideNum, "clicked", false)}
+        onClick={() => FocusOnTextArea(slideContent)}
       >
         <pre
           className={`border-1`}
