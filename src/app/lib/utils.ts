55f2f46b1d7009733803f7f9cabd5c218c4d1658
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-20 15:11:25 +0900

    feat : Added  Char Index in Text for Click Scroll Effect

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -5,8 +5,12 @@ export function parseTextDataToObjects(textData: string) {
 
   console.log(splittedData);
 
+  let charIndexInText = 0;
+
   const splittedObject = splittedData.map((data, index) => {
-    return { id: index, content: data, clicked: false };
+    const object = { id: index, content: data, charIndex: charIndexInText };
+    charIndexInText = charIndexInText + data.length + 2; // Keep track of the latest index of the splitted text
+    return object;
   });
 
   console.log(splittedObject);
@@ -14,6 +18,22 @@ export function parseTextDataToObjects(textData: string) {
 
   return splittedObject;
 }
+
+// function scrollToSentence(searchText: string) {
+//   const textarea = document.getElementById("myTextarea");
+
+//   if (textarea) {
+//     const index = textarea.value.indexOf(searchText);
+
+//     if (index !== -1) {
+//       textarea.focus();
+//       // This selects the text, which forces the browser to scroll to it
+//       textarea.setSelectionRange(index, index + searchText.length);
+//     } else {
+//       alert("Sentence not found!");
+//     }
+//   }
+// }
 
 // export function convertInnerHTMLToText(test: string) {
 //   console.log(test);
