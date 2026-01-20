ab8351274b4e5d0dad8f5e66870097254417e099
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-20 15:12:40 +0900

    chore : Added Ref and Clean Up Interace

--- src/app/ui/editor/display/display-panel.tsx
+++ src/app/ui/editor/display/display-panel.tsx
@@ -1,42 +1,30 @@
 import { parseTextDataToObjects } from "@/app/lib/utils";
 import Slide from "./slide";
 import { useEffect, useState } from "react";
-import { TextAttribute, TextObject } from "@/app/lib/type";
-
-interface DisplayPanelProps {
-  slideObject: TextObject[];
-  // handleTextObjectDataChanges: (text: string) => void;
-  textAttribute: TextAttribute;
-  handleSlideObjectChanges: <K extends keyof TextObject>(
-    index: number,
-    objectKey: K,
-    objectValue: TextObject[K],
-  ) => void;
-}
+import { DisplayPanelProps, TextAttribute, TextObject } from "@/app/lib/type";
 
 const DisplayPanel: React.FC<DisplayPanelProps> = ({
   slideObject,
-  // handleTextObjectDataChanges,
   textAttribute,
-  handleSlideObjectChanges,
+  textAreaRef,
 }) => {
-  // useEffect(() => {
-  //   handleTextObjectDataChanges(slideObject);
-  // }, [slideObject]);
-
   const slideMaxNum = slideObject.length;
 
   return (
     <>
-      <div className="border-1 p-2 gap-1 h-fit w-[80%] flex flex-col gap-1 ">
+      <div
+        className="border-1 p-2 gap-1 h-fit w-[80%] flex flex-col gap-1 "
+        style={{ scrollBehavior: "smooth" }}
+      >
         {slideObject?.map((slideData) => (
           <Slide
             key={slideData.id}
             slideNum={slideData.id}
             slideMaxNum={slideMaxNum}
             slideContent={slideData.content}
+            slideTextCharIndex={slideData.charIndex}
             textAttribute={textAttribute}
-            handleSlideObjectChanges={handleSlideObjectChanges}
+            textAreaRef={textAreaRef}
           />
         ))}
       </div>
