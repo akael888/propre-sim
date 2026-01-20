8c3b8683953b9b8de378b3f38ddd53ccd11a0ccd
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-20 16:39:39 +0900

    feat : Added Slide Move when Clicked on Text Area Feature

--- src/app/ui/editor/text-area/text-area.tsx
+++ src/app/ui/editor/text-area/text-area.tsx
@@ -1,16 +1,15 @@
 "use client";
-
-// import { convertInnerHTMLToText, insertDataBlock } from "@/app/lib/utils";
 import { useState, RefObject } from "react";
-import TextBlock from "./text-block";
-import { TextAreaProps, TextAreaSectionProps, TextObject } from "@/app/lib/type";
+import { TextAreaProps } from "@/app/lib/type";
 
 const TextArea: React.FC<TextAreaProps> = ({
   textData,
+  textObject,
   handleTextDataChanges,
   textAreaRef,
 }) => {
   const [textValue, setTextValue] = useState(textData);
+  const slidePositionList = textObject?.map((object) => object.charIndex);
 
   const handleTextValueChanges = (
     e: React.ChangeEvent<HTMLTextAreaElement>,
@@ -18,6 +17,31 @@ const TextArea: React.FC<TextAreaProps> = ({
     setTextValue(e.target.value);
     handleTextDataChanges(e.target.value);
     localStorage.setItem("TEXT_AREA_DATA", e.target.value);
+  };
+
+  const clickToScrollSlides = () => {
+    const refClicked = textAreaRef.current;
+    if (!refClicked) {
+      return;
+    }
+
+    const indexClicked = refClicked.selectionStart;
+
+    const indexIdToScroll = slidePositionList.findIndex((pos) => indexClicked < pos);
+
+    let idToScroll = null;
+    if (indexIdToScroll) {
+      idToScroll = slidePositionList[indexIdToScroll-1];
+    }
+    console.log(indexClicked);
+    console.log(slidePositionList);
+
+    console.log(indexIdToScroll);
+    console.log(idToScroll);
+
+    document.getElementById(`${idToScroll}`)?.scrollIntoView({
+      behavior: "smooth",
+    });
   };
 
   return (
@@ -36,6 +60,7 @@ const TextArea: React.FC<TextAreaProps> = ({
           value={textValue}
           onChange={handleTextValueChanges}
           placeholder="Type here.."
+          onClick={clickToScrollSlides}
           ref={textAreaRef}
         />
       </div>
