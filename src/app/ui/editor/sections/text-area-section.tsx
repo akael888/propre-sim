9bae4b60290dcd42998f3ec716d3cb1d85a0bc56
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-20 16:39:11 +0900

    chore : Added Props

--- src/app/ui/editor/sections/text-area-section.tsx
+++ src/app/ui/editor/sections/text-area-section.tsx
@@ -4,14 +4,19 @@ import { RefObject } from "react";
 
 const TextAreaSection: React.FC<TextAreaSectionProps> = ({
   textData,
+  textObject,
   handleTextDataChanges,
   textAreaRef,
 }) => {
+
+
+
   return (
     <>
       <div className="w-full h-full flex justify-center items-center border-1 p-1 overflow-y-auto md:order-none order-2">
         <TextArea
           textData={textData}
+          textObject={textObject}
           handleTextDataChanges={handleTextDataChanges}
           textAreaRef={textAreaRef}
         />
