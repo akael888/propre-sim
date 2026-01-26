65a4b97ac6285ada6a9d5ecf7f4e8d7d999f5f50
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-26 16:00:48 +0900

    feat : Added Delete Selected Theme and Reset Theme to Default

--- src/app/ui/editor/display/option/option-theme-menu.tsx
+++ src/app/ui/editor/display/option/option-theme-menu.tsx
@@ -1,6 +1,6 @@
 import { defaultThemeCollection } from "@/app/lib/data";
 import { OptionThemeMenuProp, TextAttribute, Theme } from "@/app/lib/type";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function OptionThemeMenu({
   textAttribute,
@@ -22,12 +22,23 @@ export default function OptionThemeMenu({
   const [selectedTheme, setSelectedTheme] = useState(themeCollection[0]);
   const [createdThemeName, setCreatedThemeName] = useState("");
 
-  const loadingSelectedTheme = (themeSettings: TextAttribute) => {
+  const loadSelectedTheme = (themeSettings: TextAttribute) => {
     handleTextAttributeObjectChanges(themeSettings);
+  };
+
+  const deleteSelectedTheme = (deletedTheme: Theme) => {
+    const updatedCollection = themeCollection.filter(
+      (theme) => theme.themeID !== deletedTheme.themeID,
+    );
+    setThemeCollection(updatedCollection);
+    localStorage.setItem("THEME_COLLECTION", JSON.stringify(updatedCollection));
   };
 
   const clearLocalTheme = () => {
     localStorage.removeItem("THEME_COLLECTION");
+  };
+
+  const resetThemeToDefault = () => {
     setThemeCollection(defaultThemeCollection);
   };
 
@@ -46,32 +57,50 @@ export default function OptionThemeMenu({
     <>
       <div className="p-1">
         <div>Theme:</div>
-        <div className="p-1 flex gap-2">
-          <select
-            onChange={(e) => {
-              const themeID = Number(e.currentTarget.value);
-              setSelectedTheme(themeCollection[themeID]);
-            }}
-            value={selectedTheme.themeID}
-          >
-            {themeCollection.map((theme, index) => (
-              <option key={index} value={theme.themeID}>
-                {theme.themeName}
-              </option>
-            ))}
-          </select>
-          <button
-            className="border-1 p-1 hover:bg-foreground"
-            onClick={() => loadingSelectedTheme(selectedTheme.textAttribute)}
-          >
-            Load Theme
-          </button>
-          <button
-            className="border-1 p-1 hover:bg-foreground"
-            onClick={clearLocalTheme}
-          >
-            Clear Local Theme
-          </button>
+        <div className="p-1 flex flex-col gap-2">
+          <div className="w-full flex">
+            <select
+              className="h-full"
+              onChange={(e) => {
+                const themeID = Number(e.currentTarget.value);
+                setSelectedTheme(themeCollection[themeID]);
+              }}
+              value={selectedTheme.themeID}
+            >
+              {themeCollection.map((theme, index) => (
+                <option key={index} value={theme.themeID}>
+                  {theme.themeName}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="gap-2 flex">
+            <button
+              className="border-1 p-1 hover:bg-foreground bg-green-500"
+              onClick={() => loadSelectedTheme(selectedTheme.textAttribute)}
+            >
+              Load Selected Theme
+            </button>
+            <button
+              className="border-1 p-1 hover:bg-foreground bg-red-500"
+              onClick={() => deleteSelectedTheme(selectedTheme)}
+            >
+              Delete Selected Theme
+            </button>
+            <button
+              className="border-1 p-1 hover:bg-foreground bg-orange-500"
+              onClick={clearLocalTheme}
+            >
+              Clear Local Theme
+            </button>
+            <button
+              className="border-1 p-1 hover:bg-foreground bg-gray-500"
+              onClick={resetThemeToDefault}
+            >
+              Reset Theme to Default
+            </button>
+          </div>
         </div>
         <div className="p-1 flex gap-2">
           <input
