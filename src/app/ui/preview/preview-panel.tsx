eef8a10edf3188003c32afdbf40ec7b097b45da7
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-02-08 17:51:06 +0900

    chore : Scrapped IsOpen Feature

--- src/app/ui/preview/preview-panel.tsx
+++ src/app/ui/preview/preview-panel.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { TextAttribute, TextObject } from "@/app/lib/type";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import DisplayPanel from "../editor/display/display-panel";
 
 export default function PreviewPanel({
@@ -13,6 +13,8 @@ export default function PreviewPanel({
 }) {
   const mainPanelRef = useRef<HTMLDivElement>(null);
   const sidePanelRef = useRef<HTMLDivElement>(null);
+
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleScroll = (
     scrollingDiv: HTMLDivElement,
@@ -53,14 +55,17 @@ export default function PreviewPanel({
   return (
     <>
       <div
-        className="col-span-3 flex w-full justify-center h-screen overflow-y-auto"
+        className={`col-span-3 flex w-full justify-center h-screen overflow-y-auto p-10`}
+        // style={{ gridColumn: `${isOpen ? "span 3" : "span 3"}` }}
         ref={mainPanelRef}
       >
         <DisplayPanel slideObject={slideObject} textAttribute={textAttribute} />
       </div>
+
       <div
         className="h-screen col-span-1 w-full overflow-y-auto flex justify-center"
         ref={sidePanelRef}
+        // style={{ display: `${isOpen ? "flex" : "none"}` }}
       >
         <DisplayPanel slideObject={slideObject} textAttribute={textAttribute} />
       </div>
