149c1a12229c040a7a4b63a46cc16bff93c9db76
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-02-08 09:36:04 +0900

    fix : Temporary Disable Endpoitns

--- src/app/api/u/[userID]/slide/[slideID]/route.ts
+++ src/app/api/u/[userID]/slide/[slideID]/route.ts
@@ -1,17 +1,17 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export async function GET(
-  req: Request,
-  { params }: { params: { userID: string; slideID: string } },
-) {
-  const url = await params;
+// export async function GET(
+//   req: Request,
+//   { params }: { params: { userID: string; slideID: string } },
+// ) {
+//   const url = await params;
 
 
-  return NextResponse.json(
-    {
-      data: null,
-      message: `Connected to Slide (${url.slideID}) for this user: ${url.userID}`,
-    },
-    { status: 200, statusText: "Connected" },
-  );
-}
+//   return NextResponse.json(
+//     {
+//       data: null,
+//       message: `Connected to Slide (${url.slideID}) for this user: ${url.userID}`,
+//     },
+//     { status: 200, statusText: "Connected" },
+//   );
+// }
--- src/app/api/u/[userID]/slide/route.ts
+++ src/app/api/u/[userID]/slide/route.ts
@@ -1,12 +1,12 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export async function GET(
-  req: Request,
-  { params }: { params: { userID: string } },
-) {
-  const url = await params;
-  return NextResponse.json(
-    { data: null, message: `Connected to Slide for this user: ${url.userID}` },
-    { status: 200, statusText: "Connected" },
-  );
-}
+// export async function GET(
+//   req: Request,
+//   { params }: { params: { userID: string } },
+// ) {
+//   const url = await params;
+//   return NextResponse.json(
+//     { data: null, message: `Connected to Slide for this user: ${url.userID}` },
+//     { status: 200, statusText: "Connected" },
+//   );
+// }
