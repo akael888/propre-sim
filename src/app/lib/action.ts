ef789d67478b679494280e51aaee884187bb1b85
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-02-08 17:49:55 +0900

    feat : Added Update Slide Data Function and Adjusted Submit Slide Data

--- src/app/lib/action.ts
+++ src/app/lib/action.ts
@@ -1,6 +1,7 @@
 // app/actions.ts
 "use server";
 import { neon } from "@neondatabase/serverless";
+import { redirect } from "next/navigation";
 
 export async function getData() {
   if (process.env.DATABASE_URL) {
@@ -19,16 +20,20 @@ export async function submitSlideData(formData: FormData) {
     .replace(/\r/g, "\n")
     .replace(/\f/g, "\n");
 
+  let data = [{ id: 1 }];
+
   try {
     if (process.env.DATABASE_URL) {
       const sql = neon(process.env.DATABASE_URL);
-      const data = await sql`INSERT INTO slide (title,description,textdata)
-      VALUES (${title},${description},${cleanedText})`;
+      data = await sql`INSERT INTO slide (title,description,textdata)
+      VALUES (${title},${description},${cleanedText}) RETURNING id`;
+      console.log(data);
       console.log(`Data ${data} is submitted! `);
     }
   } catch (error) {
     console.log(error);
   }
+  redirect(`/slide/${data[0].id}/edit`);
 }
 
 export async function getSlideData(slideID: string) {
@@ -47,5 +52,27 @@ export async function getSlideData(slideID: string) {
   } catch (error) {
     console.log(error);
     return null;
+  }
+}
+
+export async function updateSlideData(formData: FormData) {
+  const title = formData.get("title") as string;
+  const description = formData.get("description") as string;
+  const textdata = formData.get("textdata") as string;
+  const slideID = formData.get("slideID") as string;
+  const cleanedText = textdata
+    .replace(/\r\n/g, "\n")
+    .replace(/\r/g, "\n")
+    .replace(/\f/g, "\n");
+
+  try {
+    if (process.env.DATABASE_URL) {
+      const sql = neon(process.env.DATABASE_URL);
+      const data =
+        await sql`UPDATE slide SET title = ${title}, description = ${description}, textdata = ${cleanedText} WHERE id=${slideID}`;
+      console.log(`Data ${data} is Updated! `);
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
