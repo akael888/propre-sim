6640aae6ac3d6c4b73a1981df2897461b1d1d977
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-02-08 17:51:23 +0900

    feat : Added Edit Nav to go Back to The Edit Mode

--- src/app/ui/preview/preview.tsx
+++ src/app/ui/preview/preview.tsx
@@ -2,6 +2,7 @@ import { getSlideData } from "@/app/lib/action";
 import { parseTextDataToObjects } from "@/app/lib/utils";
 import { defaultTextAttributeData } from "@/app/lib/data";
 import PreviewPanel from "./preview-panel";
+import Link from "next/link";
 
 export default async function Preview({ slideID }: { slideID: string }) {
   const stored = await getSlideData(slideID);
@@ -21,11 +22,17 @@ export default async function Preview({ slideID }: { slideID: string }) {
   return (
     <>
       <div className="w-full grid grid-cols-4 justify-center text-black bg-gray-600 relative">
-        <div className="absolute top-0 bg-background/50 text-foreground w-full z-100 p-1">
+        <div className="absolute top-0 bg-background/50 text-foreground w-full z-100 p-1 inline-flex gap-2">
           {" "}
           <p className="font-bold">{slideData?.title}</p>
           {slideData?.description ? <p> - </p> : null}
           <p>{slideData?.description}</p>
+          <Link
+            href={`/slide/${slideID}/edit`}
+            className="border-1 bg-yellow-500 hover:bg-yellow-300 p-1 text-background"
+          >
+            Edit
+          </Link>
         </div>
         <PreviewPanel slideObject={slideObject} textAttribute={textAttribute} />
       </div>
