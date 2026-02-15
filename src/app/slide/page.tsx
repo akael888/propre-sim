e4b8023cd57d58e695021ffa582d94267c7894a6
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-02-16 00:04:25 +0900

    feat : Slide UI Expermient

--- src/app/slide/page.tsx
+++ src/app/slide/page.tsx
@@ -18,24 +18,24 @@ export default async function SlideManagerPage() {
           </Link>
         </div>
       </div>
-      <div className="flex flex-row gap-5 ">
+      <div className="flex flex-row gap-5 h-fit">
         {slideData?.map((value, index) => {
           return (
             <div
-              className="border-1 bg-foreground w-[50%] text-background text-center h-[10%] max-h-[10%] overflow-y-hidden p-2"
+              className="border-1 bg-foreground w-[50%] text-background text-center overflow-y-hidden p-2 h-fit"
               key={index}
             >
-              <div className="grid grid-cols-3 bg-pink-200 h-[50%]">
-                <div className="flex flex-col">
+              <div className="flex flex-col bg-pink-200">
+                <div className="flex flex-col p-2 h-fit">
                   <h3 className="text-2xl font-bold">{value.title}</h3>
                   <p className=""> {value.description}</p>
                 </div>
-                <div className="p-1 h-fit max-h-[10%] overflow-y-auto">
-                  <div className="border-1 text-left w-full p-1 overflow-y-auto">
+                <div className="flex p-1 max-h-[5%] p-2 h-full">
+                  <div className="border-1 text-left w-full p-1 overflow-y-auto h-full">
                     {value.textdata}
                   </div>
                 </div>
-                <div className="flex flex-col p-1 justify-center items-center gap-2 h-fit">
+                <div className="flex flex-col p-2 justify-center items-center gap-2 h-fit">
                   <Link
                     href={`/slide/${value.id}/edit`}
                     className="border-1 p-1 hover:bg-gray-300"
