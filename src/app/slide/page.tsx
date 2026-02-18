5679fe1d1b66e05bb566e03daf25e3cc6b90107b
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-02-18 12:32:09 +0900

    feat : Adjusted Slide UI and UX

--- src/app/slide/page.tsx
+++ src/app/slide/page.tsx
@@ -18,36 +18,39 @@ export default async function SlideManagerPage() {
           </Link>
         </div>
       </div>
-      <div className="flex flex-row gap-5 h-fit">
+      <div className="flex flex-col gap-5 h-fit md:flex-row">
         {slideData?.map((value, index) => {
           return (
             <div
-              className="border-1 bg-foreground w-[50%] text-background text-center overflow-y-hidden p-2 h-fit"
+              className="border-1 bg-foreground w-full text-background text-center overflow-y-hidden p-2 h-fit"
               key={index}
             >
               <div className="flex flex-col bg-pink-200">
-                <div className="flex flex-col p-2 h-fit">
-                  <h3 className="text-2xl font-bold">{value.title}</h3>
-                  <p className=""> {value.description}</p>
-                </div>
-                <div className="flex p-1 max-h-[5%] p-2 h-full">
-                  <div className="border-1 text-left w-full p-1 overflow-y-auto h-full">
-                    {value.textdata}
+                <div className="grid grid-cols-2">
+                  <div className="flex flex-col p-2 h-fit">
+                    <h3 className="text-2xl font-bold">{value.title}</h3>
+                    <p className=""> {value.description}</p>
+                  </div>
+                  <div className="flex flex-row p-2 justify-center items-center gap-2 h-full">
+                    <Link
+                      href={`/slide/${value.id}/edit`}
+                      className="border-1 p-1 hover:bg-gray-300"
+                    >
+                      Edit
+                    </Link>
+                    <Link
+                      href={`/slide/${value.id}/preview`}
+                      className="border-1 p-1 hover:bg-gray-300"
+                    >
+                      Preview
+                    </Link>
                   </div>
                 </div>
-                <div className="flex flex-col p-2 justify-center items-center gap-2 h-fit">
-                  <Link
-                    href={`/slide/${value.id}/edit`}
-                    className="border-1 p-1 hover:bg-gray-300"
-                  >
-                    Edit
-                  </Link>
-                  <Link
-                    href={`/slide/${value.id}/preview`}
-                    className="border-1 p-1 hover:bg-gray-300"
-                  >
-                    Preview
-                  </Link>
+
+                <div className="flex p-1 p-2 h-full">
+                  <div className="border-1 text-left w-full p-1 max-h-1/2 overflow-y-auto truncate">
+                    {value.textdata ? value.textdata : "Empty Data"}
+                  </div>
                 </div>
               </div>
             </div>
