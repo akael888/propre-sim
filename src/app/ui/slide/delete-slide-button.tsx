cd10e86e6dbe1ffaa2682eaee958334faae89dd0
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-02-28 01:10:55 +0900

    feat : Added Confirm Delete and Refresh on Delete

--- src/app/ui/slide/delete-slide-button.tsx
+++ src/app/ui/slide/delete-slide-button.tsx
@@ -1,15 +1,38 @@
 "use client";
 import { deleteSlideData } from "@/app/lib/action";
 import { SlideButtonsProp } from "../../lib/type";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 const DeleteSlideButton: React.FC<SlideButtonsProp> = ({ slideID }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const router = useRouter();
+  const deleteSlide = () => {
+    deleteSlideData(slideID);
+    setIsOpen(!isOpen);
+    router.refresh();
+  };
+
   return (
     <>
       <button
         className="border-1 p-1 hover:bg-gray-300 bg-red-300"
-        onClick={() => deleteSlideData(slideID)}
+        onClick={() => setIsOpen(!isOpen)}
       >
         Delete
       </button>
+      {isOpen ? (
+        <>
+          <div>
+            <button
+              className="border-1 p-1 hover:bg-gray-300 bg-red-400"
+              onClick={deleteSlide}
+            >
+              Confirm Delete
+            </button>
+          </div>
+        </>
+      ) : null}
     </>
   );
 };
