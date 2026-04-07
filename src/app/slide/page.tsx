e74694c2ebe75765ef52201c80e550520776645a
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-04-07 22:17:45 +0900

    feat : Added Empty Data State and getData using User ID

--- src/app/slide/page.tsx
+++ src/app/slide/page.tsx
@@ -6,16 +6,17 @@ import { Suspense } from "react";
 import Loading from "../ui/loading";
 
 export default async function SlideManagerPage() {
-  const slideData = await getData();
   const session = await getSession();
-  const user = session.user;
+  const user = session?.user;
+  const slideData = await getData(user?.id);
+  console.log(user);
 
   return (
     <>
       <div className="flex justify-between p-1 bg-background/80 sticky top-0 text-white">
         <div className="flex justify-center items-center font-bold">
           {" "}
-          Slide Menu: Hi {user?.name}!{" "}
+          Slide Menu: Hi {user?.name}!
         </div>
         <div className="gap-1 flex">
           <Link href="/" className="border-1 p-1 hover:bg-gray-300 bg-gray-500">
@@ -29,56 +30,64 @@ export default async function SlideManagerPage() {
           </Link>
         </div>
       </div>
-      <div className="gap-5 h-fit md:grid-cols-3 md:grid flex flex-col ">
+      <div className="gap-5 h-full md:grid-cols-3 md:grid flex flex-col bg-gray-200 p-2">
         <Suspense fallback={<Loading />}>
-          {slideData?.map((value, index) => {
-            return (
-              <div
-                className="border-1 bg-foreground w-full text-background text-center overflow-y-hidden p-2 h-fit"
-                key={index}
-              >
-                <div className="flex flex-col bg-pink-200">
-                  <div className="2xl:grid 2xl:grid-cols-2 flex flex-col">
-                    <div className="flex flex-col p-2 h-fit">
-                      <h3 className="text-2xl font-bold truncate">
-                        {value.title}
-                      </h3>
-                      <p className=""> {value.description}</p>
+          {slideData ? (
+            slideData?.map((value, index) => {
+              return (
+                <div
+                  className="border-1 bg-foreground w-full text-background text-center overflow-y-hidden p-2 h-fit"
+                  key={index}
+                >
+                  <div className="flex flex-col bg-pink-200">
+                    <div className="2xl:grid 2xl:grid-cols-2 flex flex-col">
+                      <div className="flex flex-col p-2 h-fit">
+                        <h3 className="text-2xl font-bold truncate">
+                          {value.title}
+                        </h3>
+                        <p className=""> {value.description}</p>
+                      </div>
+                      <div className="flex flex-row p-2 justify-center items-center gap-2 h-full">
+                        <Link
+                          href={`/slide/${value.id}/edit`}
+                          className="border-1 p-1 hover:bg-gray-300 bg-yellow-300"
+                        >
+                          Edit
+                        </Link>
+                        <Link
+                          href={`/slide/${value.id}/preview`}
+                          className="border-1 p-1 hover:bg-gray-300 bg-blue-300"
+                        >
+                          Preview
+                        </Link>
+                        <DeleteSlideButton slideID={value.id} />
+                      </div>
                     </div>
-                    <div className="flex flex-row p-2 justify-center items-center gap-2 h-full">
-                      <Link
-                        href={`/slide/${value.id}/edit`}
-                        className="border-1 p-1 hover:bg-gray-300 bg-yellow-300"
-                      >
-                        Edit
-                      </Link>
-                      <Link
-                        href={`/slide/${value.id}/preview`}
-                        className="border-1 p-1 hover:bg-gray-300 bg-blue-300"
-                      >
-                        Preview
-                      </Link>
-                      <DeleteSlideButton slideID={value.id} />
-                    </div>
-                  </div>
 
-                  <div className="flex p-1 p-2 h-full flex-col">
-                    <div className="border-1 text-left w-full p-1 max-h-1/2 overflow-y-auto truncate">
-                      {value.textdata ? value.textdata : "Empty Data"}
-                    </div>
-                    <div className="flex flex-row justify-between p-1">
-                      <p>
-                        {value.textdata
-                          ? value.textdata.length + " Letters"
-                          : "Empty Data"}
-                      </p>
-                      <p>Created At : {value.created_at.toISOString()}</p>
+                    <div className="flex p-1 p-2 h-full flex-col">
+                      <div className="border-1 text-left w-full p-1 max-h-1/2 overflow-y-auto truncate">
+                        {value.textdata ? value.textdata : "Empty Data"}
+                      </div>
+                      <div className="flex flex-row justify-between p-1">
+                        <p>
+                          {value.textdata
+                            ? value.textdata.length + " Letters"
+                            : "Empty Data"}
+                        </p>
+                        {value.created_at && (
+                          <p>Created At : {value.created_at.toISOString()}</p>
+                        )}
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })
+          ) : (
+            <>
+              <div className="text-black p-2">Empty Data</div>
+            </>
+          )}
         </Suspense>
       </div>
     </>
