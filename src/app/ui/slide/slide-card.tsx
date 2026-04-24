31f1c3704f37c2ccadf61021c26348f2f90850fc
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-04-24 22:14:41 +0900

    fix : Added Validation before accessing userSlideObject.id

--- src/app/ui/slide/slide-card.tsx
+++ src/app/ui/slide/slide-card.tsx
@@ -5,9 +5,7 @@ import { SlideCardProp } from "@/app/lib/type";
 const SlideCard: React.FC<SlideCardProp> = ({ index, userSlideOBject }) => {
   return (
     <>
-      <div
-        className="border-1 border-gray-500 bg-foreground h-full w-full text-background text-center overflow-y-hidden p-2 flex flex-col hover:bg-gray-100 shadow-xl hover:-translate-y-1 hover:border-white rounded-xl"
-      >
+      <div className="border-1 border-gray-500 bg-foreground h-full w-full text-background text-center overflow-y-hidden p-2 flex flex-col hover:bg-gray-100 shadow-xl hover:-translate-y-1 hover:border-white rounded-xl">
         <div className="flex flex-col h-full">
           <div className="2xl:grid 2xl:grid-cols-1 flex flex-col h-full">
             <div className="flex flex-col p-2 h-fit">
@@ -35,25 +33,33 @@ const SlideCard: React.FC<SlideCardProp> = ({ index, userSlideOBject }) => {
                   : "Empty Data"}
               </p>
               {userSlideOBject.created_at && (
-                <p>Created on {new Date(userSlideOBject.created_at).toDateString()}</p>
+                <p>
+                  Created on{" "}
+                  {new Date(userSlideOBject.created_at).toDateString()}
+                </p>
               )}
             </div>
           </div>
         </div>{" "}
         <div className="flex flex-row p-2 justify-end items-center gap-2 h-full">
-          <Link
-            href={`/slide/${userSlideOBject.id}/edit`}
-            className="border-1 p-1 hover:bg-gray-300 bg-yellow-300/50 w-20 rounded-sm"
-          >
-            Edit
-          </Link>
-          <Link
-            href={`/slide/${userSlideOBject.id}/preview`}
-            className="border-1 p-1 hover:bg-gray-300 bg-blue-300/50 w-20 rounded-sm"
-          >
-            Preview
-          </Link>
-          <DeleteSlideButton slideID={userSlideOBject.id} />
+          {userSlideOBject.id && (
+            <>
+              {" "}
+              <Link
+                href={`/slide/${userSlideOBject.id}/edit`}
+                className="border-1 p-1 hover:bg-gray-300 bg-yellow-300/50 w-20 rounded-sm"
+              >
+                Edit
+              </Link>
+              <Link
+                href={`/slide/${userSlideOBject.id}/preview`}
+                className="border-1 p-1 hover:bg-gray-300 bg-blue-300/50 w-20 rounded-sm"
+              >
+                Preview
+              </Link>
+              <DeleteSlideButton slideID={userSlideOBject.id} />
+            </>
+          )}
         </div>
       </div>
     </>
