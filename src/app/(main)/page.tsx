4f291e82567d59b0c9077b6af3bb54a7bbf53dc8
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-04-25 09:48:30 +0900

    feat : Home Page Revamp

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,18 +3,21 @@ import Link from "next/link";
 export default function MainPage() {
   return (
     <>
-      <div className="w-full h-screen flex flex-col justify-center items-center bg-foreground text-background gap-3">
+      <div className="bg-foreground/80 p-1 xl:w-200 w-full h-100 flex flex-col justify-center items-center text-center gap-2 rounded-sm">
         <p className="text-2xl text-start font-bold">Welcome to ProPre Sim</p>
         <p className="text-sm">
           a ProPresenter Slide Simulator using Text Based Editor
         </p>
-        <p>
-          Click <strong> [To Editor]</strong> button on the Header ↑
-        </p>
+        <Link
+          href="/editor"
+          className="border-1 p-1 bg-foreground text-background hover:bg-yellow-700"
+        >
+          To Editor
+        </Link>
         <div className="border-1 p-1 text-center bg-background text-foreground">
           <p className="text-sm italic">In Development..</p>
           <p className="text-sm">
-            Contribute here → 
+            Contribute here →
             <Link
               href="https://github.com/akael888/propre-sim"
               className="underline hover:font-bold italic p-1"
