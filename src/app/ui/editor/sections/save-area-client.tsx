72cbc6f8641b4beb20f4970b120396ca83882b41
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-05-02 11:45:42 +0900

    feat : Refactor Save Area

---
+++ src/app/ui/editor/sections/save-area-client.tsx
@@ -0,0 +1,144 @@
+"use client";
+import { updateSlideData } from "@/app/lib/action";
+import { User } from "next-auth";
+import Link from "next/link";
+
+export default function SaveAreaClient({
+  slideID,
+  user,
+  submitAction,
+  textAreaData,
+  slideData,
+  slideDataStatic,
+  handleSlideDataObjectChanges,
+  isTextAreaNotChanged,
+}: {
+  slideID?: string;
+  user: ({ id: string } & User) | undefined;
+  submitAction?: ((formData: FormData) => void) | string;
+  textAreaData: string;
+  slideData: {
+    title: string;
+    description: string;
+  };
+  slideDataStatic: {
+    title: string;
+    description: string;
+  };
+  handleSlideDataObjectChanges: (data: {
+    title: string;
+    description: string;
+  }) => void;
+  isTextAreaNotChanged: boolean;
+}) {
+  return (
+    <>
+      <div className="flex flex-col gap-5 max-w-screen w-full p-1 ">
+        {user ? (
+          <form
+            action={slideID ? updateSlideData : submitAction}
+            className="gap-2 flex flex-row justify-between max-w-screen w-full"
+          >
+            <div className="flex flex-col w-full h-full">
+              <input
+                type="text"
+                name="title"
+                required
+                placeholder="Enter Slide Title"
+                maxLength={30}
+                minLength={3}
+                className="w-full font-bold text-center hover:bg-background/30"
+                defaultValue={slideID ? slideDataStatic.title : ""}
+                onChange={
+                  slideID
+                    ? (e) =>
+                        handleSlideDataObjectChanges({
+                          ...slideData,
+                          title: e.target.value,
+                        })
+                    : undefined
+                }
+              />
+              <input
+                type="text"
+                name="description"
+                placeholder="Enter Slide Description"
+                maxLength={50}
+                minLength={3}
+                required
+                className="w-full italic text-center hover:bg-background/30 "
+                defaultValue={slideID ? slideDataStatic.description : ""}
+                onChange={
+                  slideID
+                    ? (e) =>
+                        handleSlideDataObjectChanges({
+                          ...slideData,
+                          description: e.target.value,
+                        })
+                    : undefined
+                }
+              />
+              <input
+                type="hidden"
+                name="textdata"
+                value={textAreaData}
+                readOnly
+              />
+              {slideID ? (
+                <input type="hidden" name="slideID" value={slideID} readOnly />
+              ) : null}
+            </div>
+            <div className="flex flex-row w-full gap-2">
+              <button
+                className="border-1 p-1 bg-green-500 hover:bg-green-600 disabled:bg-background disabled:text-foreground w-full rounded-md hover:font-bold"
+                type="submit"
+                disabled={slideID ? isTextAreaNotChanged : false}
+              >
+                {slideID
+                  ? `Save${isTextAreaNotChanged ? "" : "*"}`
+                  : "Save Data"}
+              </button>
+              {slideID && (
+                <Link
+                  href={`/slide/${slideID}/preview`}
+                  className="border-1 bg-blue-200 p-1 w-full items-center flex justify-center hover:bg-blue-800 rounded-md hover:font-bold"
+                >
+                  Preview
+                </Link>
+              )}
+
+              <Link
+                href="/slide"
+                className="border-1 p-1 bg-foreground text-background hover:bg-blue-700 w-full text-center justify-center items-center flex rounded-md hover:font-bold"
+              >
+                Slide Collections
+              </Link>
+            </div>
+          </form>
+        ) : (
+          <>
+            <div className="flex flex-row gap-2">
+              <div className="p-1 font-bold">
+                Login or Register to Save Data
+              </div>
+              <div className="flex flex-row gap-1 justify-end items-center w-full">
+                <Link
+                  href="/login"
+                  className="border-1 p-1 bg-foreground text-background hover:bg-blue-700 w-full text-center justify-center items-center flex rounded-md hover:font-bold"
+                >
+                  Login
+                </Link>
+                <Link
+                  href="/register"
+                  className="border-1 p-1 bg-foreground text-background hover:bg-blue-700 w-full text-center justify-center items-center flex rounded-md hover:font-bold"
+                >
+                  Register
+                </Link>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+}
--- src/app/ui/editor/sections/save-area-section.tsx
+++ src/app/ui/editor/sections/save-area-section.tsx
@@ -1,16 +1,13 @@
 import { getSession, submitSlideData, updateSlideData } from "@/app/lib/action";
-import { User } from "next-auth";
-import Link from "next/link";
-import { use, useState } from "react";
+import SaveAreaClient from "./save-area-client";
 
-function SaveSection({
+async function SaveSection({
   slideID,
   textAreaData,
   slideData,
   slideDataStatic,
   handleSlideDataObjectChanges,
   isTextAreaNotChanged,
-  user,
 }: {
   slideID?: string;
   textAreaData: string;
@@ -27,118 +24,23 @@ function SaveSection({
     description: string;
   }) => void;
   isTextAreaNotChanged: boolean;
-  user?: User;
 }) {
+  const session = await getSession();
+  const user = session?.user;
   const submitSlideDatawithID = user?.id && submitSlideData.bind(null, user.id);
 
   return (
     <>
-      <div className="flex flex-col gap-5 max-w-screen w-full p-1 ">
-        {user ? (
-          <form
-            action={slideID ? updateSlideData : submitSlideDatawithID}
-            className="gap-2 flex flex-row justify-between max-w-screen w-full"
-          >
-            <div className="flex flex-col w-full h-full">
-              <input
-                type="text"
-                name="title"
-                required
-                placeholder="Enter Slide Title"
-                maxLength={30}
-                minLength={3}
-                className="w-full font-bold text-center hover:bg-background/30"
-                defaultValue={slideID ? slideDataStatic.title : ""}
-                onChange={
-                  slideID
-                    ? (e) =>
-                        handleSlideDataObjectChanges({
-                          ...slideData,
-                          title: e.target.value,
-                        })
-                    : undefined
-                }
-              />
-              <input
-                type="text"
-                name="description"
-                placeholder="Enter Slide Description"
-                maxLength={50}
-                minLength={3}
-                required
-                className="w-full italic text-center hover:bg-background/30 "
-                defaultValue={slideID ? slideDataStatic.description : ""}
-                onChange={
-                  slideID
-                    ? (e) =>
-                        handleSlideDataObjectChanges({
-                          ...slideData,
-                          description: e.target.value,
-                        })
-                    : undefined
-                }
-              />
-              <input
-                type="hidden"
-                name="textdata"
-                value={textAreaData}
-                readOnly
-              />
-              {slideID ? (
-                <input type="hidden" name="slideID" value={slideID} readOnly />
-              ) : null}
-            </div>
-            <div className="flex flex-row w-full gap-2">
-              <button
-                className="border-1 p-1 bg-green-500 hover:bg-green-600 disabled:bg-background disabled:text-foreground w-full rounded-md hover:font-bold"
-                type="submit"
-                disabled={slideID ? isTextAreaNotChanged : false}
-              >
-                {slideID
-                  ? `Save${isTextAreaNotChanged ? "" : "*"}`
-                  : "Save Data"}
-              </button>
-              {slideID && (
-                <Link
-                  href={`/slide/${slideID}/preview`}
-                  className="border-1 bg-blue-200 p-1 w-full items-center flex justify-center hover:bg-blue-800 rounded-md hover:font-bold"
-                >
-                  Preview
-                </Link>
-              )}
-
-              <Link
-                href="/slide"
-                className="border-1 p-1 bg-foreground text-background hover:bg-blue-700 w-full text-center justify-center items-center flex rounded-md hover:font-bold"
-              >
-                Slide Collections
-              </Link>
-            </div>
-          </form>
-        ) : (
-          <>
-            <div className="flex flex-row gap-2">
-              <div className="p-1 font-bold">
-                Login or Register to Save Data
-              </div>
-              <div className="flex flex-row gap-1 justify-end items-center w-full">
-                <Link
-                  href="/login"
-                  className="border-1 p-1 bg-foreground text-background hover:bg-blue-700 w-full text-center justify-center items-center flex rounded-md hover:font-bold"
-                >
-                  Login
-                </Link>
-                <Link
-                  href="/register"
-                  className="border-1 p-1 bg-foreground text-background hover:bg-blue-700 w-full text-center justify-center items-center flex rounded-md hover:font-bold"
-                >
-                  Register
-                </Link>
-              </div>
-            </div>
-          </>
-        )}
-      </div>
+      <SaveAreaClient
+        slideID={slideID}
+        user={user}
+        submitAction={submitSlideDatawithID}
+        textAreaData={textAreaData}
+        slideData={slideData}
+        slideDataStatic={slideDataStatic}
+        handleSlideDataObjectChanges={handleSlideDataObjectChanges}
+        isTextAreaNotChanged={isTextAreaNotChanged}
+      />
     </>
   );
 }
