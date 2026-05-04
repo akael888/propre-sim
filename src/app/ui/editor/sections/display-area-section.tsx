94f500d6af799b6f42b6b7bc586234312212e459
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-05-04 22:50:49 +0900

    feat : Added Option Modal to the Section

--- src/app/ui/editor/sections/display-area-section.tsx
+++ src/app/ui/editor/sections/display-area-section.tsx
@@ -4,6 +4,7 @@ import DisplayOptions from "../display/display-options";
 import { DisplayAreaSectionProps, TextAttribute } from "../../../lib/type";
 import { defaultTextAttributeData } from "../../../lib/data";
 import Help from "../display/help";
+import OptionModal from "../display/display-option-modal";
 
 const DisplayAreaSection: React.FC<DisplayAreaSectionProps> = ({
   slideObject,
@@ -44,10 +45,36 @@ const DisplayAreaSection: React.FC<DisplayAreaSectionProps> = ({
     setDisplayedTextAttribute(newObject);
   };
 
+  const [isOptionOpen, setIsOptionOpen] = useState(false);
+
   return (
     <>
-      <div className="border-1 w-full md:max-h-screen bg-gray-500 justify-center flex flex-row  md:order-none order-1 max-h-full ">
-        <div className="flex-1 overflow-y-auto flex flex-row h-full justify-center">
+      {isOptionOpen && (
+        <>
+          <OptionModal
+            handleTextAttributeChanges={handleTextAttributeChanges}
+            handleTextAttributeObjectChanges={handleTextAttributeObjectChanges}
+            textAttribute={displayedTextAttribute}
+          />{" "}
+          <div
+            className="bg-background/50 w-screen h-screen fixed z-1000 md:hidden"
+            onClick={() => setIsOptionOpen(!isOptionOpen)}
+          />
+        </>
+      )}
+      <div className="border-1 w-full md:max-h-screen bg-gray-500 justify-center flex flex-row  md:order-none order-1 max-h-full relative">
+        <div className="md:hidden flex absolute bottom-0 z-100 p-1 bg-foreground/10 w-full justify-evenly gap-2">
+          <button
+            className="border-1 p-1 w-20 rounded-sm bg-foreground"
+            onClick={() => setIsOptionOpen(!isOptionOpen)}
+          >
+            Option
+          </button>
+          <button className="border-1 p-1 w-20 rounded-sm bg-foreground">
+            Help
+          </button>
+        </div>
+        <div className="flex-1 overflow-y-auto flex flex-row h-full justify-center ">
           <DisplayPanel
             slideObject={slideObject}
             textAttribute={displayedTextAttribute}
@@ -55,7 +82,7 @@ const DisplayAreaSection: React.FC<DisplayAreaSectionProps> = ({
           />{" "}
         </div>
 
-        <Help />
+        {/* <Help /> */}
       </div>
       <DisplayOptions
         handleTextAttributeChanges={handleTextAttributeChanges}
