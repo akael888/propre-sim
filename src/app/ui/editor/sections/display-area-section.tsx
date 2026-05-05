1cb8db46e2047c59d5722d1bae67bb988dc65412
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-05-05 12:57:21 +0900

    feat : Added Help Modal for Mobile Screen

--- src/app/ui/editor/sections/display-area-section.tsx
+++ src/app/ui/editor/sections/display-area-section.tsx
@@ -5,6 +5,7 @@ import { DisplayAreaSectionProps, TextAttribute } from "../../../lib/type";
 import { defaultTextAttributeData } from "../../../lib/data";
 import Help from "../display/help";
 import OptionModal from "../display/display-option-modal";
+import HelpModal from "../display/help-modal";
 
 const DisplayAreaSection: React.FC<DisplayAreaSectionProps> = ({
   slideObject,
@@ -46,6 +47,7 @@ const DisplayAreaSection: React.FC<DisplayAreaSectionProps> = ({
   };
 
   const [isOptionOpen, setIsOptionOpen] = useState(false);
+  const [isHelpOpen, setIsHelpOpen] = useState(false);
 
   return (
     <>
@@ -61,6 +63,15 @@ const DisplayAreaSection: React.FC<DisplayAreaSectionProps> = ({
             onClick={() => setIsOptionOpen(!isOptionOpen)}
           />
         </>
+      )}{" "}
+      {isHelpOpen && (
+        <>
+          <HelpModal />
+          <div
+            className="bg-background/50 w-screen h-screen fixed z-1000 md:hidden"
+            onClick={() => setIsHelpOpen(!isHelpOpen)}
+          />
+        </>
       )}
       <div className="border-1 w-full md:max-h-screen bg-gray-500 justify-center flex flex-row  md:order-none order-1 max-h-full relative">
         <div className="md:hidden flex absolute bottom-0 z-100 p-1 bg-foreground/10 w-full justify-evenly gap-2">
@@ -70,7 +81,10 @@ const DisplayAreaSection: React.FC<DisplayAreaSectionProps> = ({
           >
             Option
           </button>
-          <button className="border-1 p-1 w-20 rounded-sm bg-foreground">
+          <button
+            className="border-1 p-1 w-20 rounded-sm bg-foreground"
+            onClick={() => setIsHelpOpen(!isHelpOpen)}
+          >
             Help
           </button>
         </div>
