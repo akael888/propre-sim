f46fa86f9843287091d23182e7b833d9cdf7f724
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-06-01 22:31:12 +0900

    chore : Children with SlideAttributeProvider

--- src/app/context/app-provider.tsx
+++ src/app/context/app-provider.tsx
@@ -1,12 +1,15 @@
 import { ReactNode } from "react";
 import { TextAttributeProvider } from "./text-attribute-context";
 import { TextDataProvider } from "./text-data-context";
+import { SlideAttributeProvider } from "./slide-attribute-context";
 
 export function AppProvider({ children }: { children: ReactNode }) {
   return (
     <>
       <TextAttributeProvider>
-        <TextDataProvider>{children}</TextDataProvider>
+        <SlideAttributeProvider>
+          <TextDataProvider>{children}</TextDataProvider>
+        </SlideAttributeProvider>
       </TextAttributeProvider>
     </>
   );
